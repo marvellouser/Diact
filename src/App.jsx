15e2a7ab5ae6ad0ebb4218ae74bd760f2cad94e2
liutao <779217963@qq.com>
2021-06-17 09:59:23 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,8 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Test from './Test';
-function App() {
-  const [count, setCount] = useState(0)
+import React from './Diact';
 
-  return (
-    <div className="App">
-      <Test />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.  1
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+const con = <div>
+    <h1>title</h1>
+    <div>content</div>
+</div>
 
-export default App
+React.render(con, document.getElementById('root'))
